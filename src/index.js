0aa0438063fd7dbdf3cec16ce3ca28cf4245351d
funkaclau <cloudspg@gmail.com>
2026-03-23 03:22:26 +0000

    updated ex/imports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { handleApproveAndRun, parseAmountToWei, handleApproveAndRunWeb3, formatAmount, shortenAddress, copyToClipboard,
-    toBI, calculateShidoForMaxPurchase, convertBigIntToFloat, formatNumberWithComma
+    toBI, calculateShidoForMaxPurchase, convertBigIntToFloat, formatNumberWithCommas, multicallBalances
  } from "./utils";
 
 export {
     handleApproveAndRun,
     handleApproveAndRunWeb3,
-    formatAmount, toBI, calculateShidoForMaxPurchase, convertBigIntToFloat, parseAmountToWei, formatNumberWithComma,
-    copyToClipboard, shortenAddress
+    formatAmount, toBI, calculateShidoForMaxPurchase, convertBigIntToFloat, parseAmountToWei, formatNumberWithCommas,
+    copyToClipboard, shortenAddress, multicallBalances
 };
 
 
@@ -35,8 +35,11 @@ import {
 } from "./config";
 
 
-import {POOL_ABI, PRESET_POOLS, Q192, Q96, WSHIDO_USDC_POOL, prettySymbol, EXPLICIT_MAP, ROUTE_HINTS_BY_SYMBOL} from "./config/price"
-import {POOL_REGISTRY} from "./config/markets"
+import {POOL_ABI, PRESET_POOLS, Q192, Q96, WSHIDO_USDC_POOL, prettySymbol, EXPLICIT_MAP, ROUTE_HINTS_BY_SYMBOL, DOMINANT_GROUPS, 
+    FULL_TOKEN_LIST, QUOTER_V2, QUOTER_V2_ABI, SWAP_ROUTER_ABI, SWAP_ROUTER_V3
+
+} from "./config/price"
+import {POOL_REGISTRY, RPC_URL} from "./config/markets"
 export {
     BATCH_ABI,
     TAG,
@@ -54,5 +57,6 @@ export {
     CONTRACTS, ABIS, SC_METADATA, KIDDO_ADDRESSES, WHITELISTED_COLLECTIONS, GATEWAYS, ABI, VAULT_V1_ABI, NFT_ABI, NFT_ENUMERABLE, NFT2ME_721_ABI, 
     WHITELISTED_TOKENS,
     EXPLICIT_MAP, ROUTE_HINTS_BY_SYMBOL, TOKEN_IMAGE_MAP,
-    STAKING_POOLS, MULTICALL, POOL_REGISTRY
+    STAKING_POOLS, MULTICALL, POOL_REGISTRY, DOMINANT_GROUPS, FULL_TOKEN_LIST, QUOTER_V2, QUOTER_V2_ABI,
+    RPC_URL, SWAP_ROUTER_ABI, SWAP_ROUTER_V3
 };
